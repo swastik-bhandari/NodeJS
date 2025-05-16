287c888f01e1a647444b1f4d6bb29d4013e19378
swastik-bhandari <swastikbhandari2006@gmail.com>
2025-05-16 17:57:18 +0545

    introduction to nodeJS and learned modules like path , os

---
+++ server.js
@@ -0,0 +1,20 @@
+// console.log('hello world');
+// const os = require('os');
+// console.log(os.version());
+// console.log(os.homedir());
+// console.log(os.type());
+// console.log(__dirname);
+// console.log(__filename);
+// const path = require('path');
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+// console.log(path.parse(__filename)); // returns object 
+
+// {  ********* path.parse(__filename) return an object as below *************
+//   root: 'C:\\',
+//   dir: 'C:\\Users\\ACER\\OneDrive\\Desktop\\nodeJS',
+//   base: 'server.js',
+//   ext: '.js',
+//   name: 'server'
+// }
