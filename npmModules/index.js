d9d3f43723f649a462307ec21478de5437c9801c
swastik-bhandari <swastikbhandari2006@gmail.com>
2025-05-19 21:48:35 +0545

    topics covered - dev dependencies , production dependencies , semantic versioning , modules like nodemon , uuid

---
+++ npmModules/index.js
@@ -0,0 +1,7 @@
+// const { format } = require('date-fns');
+
+// console.log(format(new Date() , 'yyMMMMdd\tHH:mm:ss'))
+
+const {v4 } = require('uuid');  /// object desturcturing with object alias 
+
+console.log(v4());
